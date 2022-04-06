48b5b23f98b423bc5cdc42bcc067b1eb16268e40
Isaac Rodriguez <isaacrodriguez@isaacs-imac.attlocal.net>
2022-04-05 19:13:11 -0700

    testing

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql2')
+
+
+//created route for mysql2 UIR
+var connection = mysql2.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Password1!',
+  database: 'sakila'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error(err.message);
+    return;
+  }
+  console.log('We are connected to database');
+})
 
 
 /* GET home page. */
