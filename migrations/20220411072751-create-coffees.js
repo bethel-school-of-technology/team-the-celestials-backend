188641fb55d510439ab5c4deaef519396eb3a2ae
Isaac Rodriguez <isaacrodriguez@isaacs-imac.attlocal.net>
2022-04-11 00:34:02 -0700

    redid coffee and user models

---
+++ migrations/20220411072751-create-coffees.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('coffees', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      coffee - _id: {
+        type: Sequelize.INTEGER
+      },
+      nameOfCoffee: {
+        type: Sequelize.STRING
+      },
+      ingridients: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.DECIMAL
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('coffees');
+  }
+};
